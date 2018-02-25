d287373c812730cc958ab6af6b2e0e9acb3808dd
Benjamin Wasty <benny.wasty@gmail.com>
2018-02-25 23:18:40 +0100

    add xhr2 -> working proper test

--- test/gltf-loader.test.ts
+++ test/gltf-loader.test.ts
@@ -4,44 +4,27 @@ import * as sinon from 'sinon';
 
 import { GltfLoader } from '../source/gltf-loader';
 
+import * as XMLHttpRequest from 'xhr2';
+(global as any).XMLHttpRequest = XMLHttpRequest;
+
 describe('gltf-operate', () => {
-    let server;
-    beforeEach(function () {
-        server = sinon.fakeServer.create();
-        // window.XMLHttpRequest = global.XMLHttpRequest; // This line is important
-    });
-
-    afterEach(function () {
-        server.restore();
-    });
-
     it('should load normal gltf files', () => {
-        // const server = sinon.createFakeServer();
-
         const loader = new GltfLoader();
         loader.load(
             'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Box/glTF/Box.gltf',
             // onLoad
             (gltf) => {
-                console.log(gltf)
+                // console.log(gltf)
             },
             // onProgress
             (xhr: any) => {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                // console.log((xhr.loaded / xhr.total * 100) + '% loaded');
             },
             // onError
             (error) => {
-                console.error(error)
+                // console.error(error)
             },
         );
-
-        server.requests[0].respond(
-            200,
-            { "Content-Type": "application/json" },
-            JSON.stringify([{ id: 1, text: "Provide examples", done: true }])
-        );
-
-        // this.server.restore();
     });
 
     it('should load GLB files', () => {
