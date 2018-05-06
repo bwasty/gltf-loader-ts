c76b9959b563fb889c8e6f52e675674822deb03d
Benjamin Wasty <benny.wasty@gmail.com>
2018-05-06 19:05:27 +0200

    mostly fix issues with ts-node upgrade (skipped for now)

--- test/gltf-loader.test.ts
+++ test/gltf-loader.test.ts
@@ -27,17 +27,17 @@ if (process.env.SAMPLE_MODELS_BASE) {
 async function expect_asset_to_be_standard_box(asset: GltfAsset) {
     // check json
     expect(asset.gltf.asset.version).to.equal('2.0');
-    expect(asset.gltf.materials[0].name).to.equal('Red');
+    expect(asset.gltf.materials![0].name).to.equal('Red');
 
     // check buffer directly
     const buffer = await asset.bufferData.get(0);
-    expect(buffer.byteLength).to.equal(asset.gltf.buffers[0].byteLength);
+    expect(buffer.byteLength).to.equal(asset.gltf.buffers![0].byteLength);
 
     // check buffer views
     const data = await asset.bufferViewData(0);
-    expect(data.byteLength).to.equal(asset.gltf.bufferViews[0].byteLength);
+    expect(data.byteLength).to.equal(asset.gltf.bufferViews![0].byteLength);
     const data1 = await asset.bufferViewData(1);
-    expect(data1.byteLength).to.equal(asset.gltf.bufferViews[1].byteLength);
+    expect(data1.byteLength).to.equal(asset.gltf.bufferViews![1].byteLength);
 }
 
 describe('gltf-loader', function() {
@@ -45,7 +45,7 @@ describe('gltf-loader', function() {
         const loader = new GltfLoader();
         const asset = await loader.load(SAMPLE_MODELS_BASE + 'Box/glTF/Box.gltf');
 
-        expect(asset.gltf.buffers[0].uri).to.equal('Box0.bin');
+        expect(asset.gltf.buffers![0].uri).to.equal('Box0.bin');
         expect_asset_to_be_standard_box(asset);
     });
 
@@ -53,14 +53,14 @@ describe('gltf-loader', function() {
         const loader = new GltfLoader();
         const asset = await loader.load(SAMPLE_MODELS_BASE + 'Box/glTF-Binary/Box.glb');
 
-        expect(asset.gltf.buffers[0].byteLength).to.equal(648);
+        expect(asset.gltf.buffers![0].byteLength).to.equal(648);
         await expect_asset_to_be_standard_box(asset);
     });
 
     it('should load files with embedded data', async function() {
         const loader = new GltfLoader();
         const asset = await loader.load(SAMPLE_MODELS_BASE + 'Box/glTF-Embedded/Box.gltf');
-        expect(asset.gltf.buffers[0].uri).to.match(/^data:application\/octet-stream;base64,AAA/);
+        expect(asset.gltf.buffers![0].uri).to.match(/^data:application\/octet-stream;base64,AAA/);
         // NOTE: fails actually...but in the browser it works (-> issue with `xhr2`?)
         // await expect_asset_to_be_standard_box(asset);
     });
@@ -75,7 +75,7 @@ describe('gltf-loader', function() {
                 onProgressCalled = true;
             },
         );
-        const gltf = await promise;
+        await promise;
         if (!onProgressCalled) {
             throw new Error('onProgress not called');
         }
@@ -84,7 +84,7 @@ describe('gltf-loader', function() {
     it('should report errors via the onError callback', async function() {
         const loader = new GltfLoader();
         try {
-            const gltf = await loader.load(SAMPLE_MODELS_BASE + 'this/should/404');
+            await loader.load(SAMPLE_MODELS_BASE + 'this/should/404');
             throw new Error('this should have failed.');
         } catch (error) {
             expect(error).to.eql({ status: 404, statusText: 'Not Found' });
@@ -94,30 +94,30 @@ describe('gltf-loader', function() {
     it('should handle an external loadingManager correctly', async function() {
         const URL = SAMPLE_MODELS_BASE + 'Box/glTF/Box.gltf';
         let manager = new LoadingManager();
-        manager.onStart = spy((url, itemsLoaded, itemsTotal) => {
+        manager.onStart = spy((url: string, itemsLoaded: number, itemsTotal: number) => {
             expect(url).to.equal(URL);
             expect(itemsLoaded).to.equal(0);
             expect(itemsTotal).to.equal(1);
         });
-        manager.onProgress = spy((url, itemsLoaded, itemsTotal) => {
+        manager.onProgress = spy((url: string, itemsLoaded: number, itemsTotal: number) => {
             expect(url).to.equal(URL);
             expect(itemsLoaded).to.equal(1);
             expect(itemsTotal).to.equal(1);
         });
         manager.onLoad = spy(() => {});
-        manager.onError = spy((url) => {});
+        manager.onError = spy((url: string) => {});
 
         let loader = new GltfLoader(manager);
         const promise = loader.load(URL);
         expect(manager.onStart).to.have.been.called();
-        const gltf = await promise;
+        await promise;
         expect(manager.onProgress).to.have.been.called();
         expect(manager.onLoad).to.have.been.called();
         expect(manager.onError).to.have.not.been.called();
 
         // test onError
         manager = new LoadingManager();
-        manager.onError = spy((url) => {
+        manager.onError = spy((url: string) => {
             expect(url).to.equal(SAMPLE_MODELS_BASE + 'this/should/404');
         });
         loader = new GltfLoader(manager);
@@ -187,7 +187,7 @@ describe('gltf-loader', function() {
         const loader = new GltfLoader();
         const asset = await loader.load(SAMPLE_MODELS_BASE + 'BoxTextured/glTF-Binary/BoxTextured.glb');
 
-        const promise = asset.imageData.get(0);
+        asset.imageData.get(0);
         // TODO!!: img.onload is not a function??
         // img.onload();
         // await promise;
